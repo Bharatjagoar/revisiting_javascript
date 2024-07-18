14d35ce63a9c89d30e5f8eded6879e289e4358ab
Bharat Jagoar <bharatjagoar15@gmail.com>
2024-07-18 17:31:18 +0530

    hoisting variables and functions

---
+++ hoisting/index.js
@@ -0,0 +1,13 @@
+greet()
+
+
+function greet(){
+    console.log("Fsa")
+}
+
+greeting()
+
+let greeting = function(){
+    console.log("good afternoon")
+}//here hte greeting function is not hoisted because it is a variable and variables are set to undefined if values
+//are not found initially like here
